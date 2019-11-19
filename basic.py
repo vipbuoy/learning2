929088d582693e277e1c1116bb46ef831942ff86
vipbouy <aggarwalvipul47@gmail.com>
2019-11-19 16:28:26 +0530

    Initial commit

---
+++ basic.py
@@ -0,0 +1,11 @@
+#very basic
+print("Hello!! World!!")
+print('What is your name?')
+Myname=input()
+print('It is good to meet you, '+Myname)
+print('The length of your name is:')
+print(len(Myname))
+print('What is your age?')
+myAge=0
+myAge=input()
+print('you will be'+str(int(myAge)+1)+'in a year')
